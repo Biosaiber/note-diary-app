58a969b13c32d7ae490a501714ce6f81c2935b50
isubfox <samuel.liska@gmail.com>
2026-06-15 00:25:51 +0200

    Integrate logger service and log display

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, DoCheck } from '@angular/core';
 import { NoteCreateComponent } from './note-create/note-create.component';
 import { NoteListComponent } from './note-list/note-list.component';
+import { LoggerService } from './logger.service';
 
 @Component({
   selector: 'app-root',
-  imports: [NoteCreateComponent, NoteListComponent],
+  imports: [NoteCreateComponent, NoteListComponent,],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('notes-diary-app');
+export class App implements OnInit, DoCheck {
+  private loggerService = inject(LoggerService);
+
+  logMessages: string[] = [];
+
+  ngOnInit(): void {
+    this.logMessages = this.loggerService.messages;
+  }
+
+  ngDoCheck(): void {
+    this.logMessages = this.loggerService.messages;
+  }
+
 }
--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core'
+import { Injectable, inject } from '@angular/core'
 import { Note } from './note.interface'
+import { LoggerService } from './logger.service'
 
 @Injectable({
   providedIn: 'root',
@@ -7,20 +8,27 @@ import { Note } from './note.interface'
 export class NoteService {
 
   private notes: Note[] = [];
+  private loggerService = inject(LoggerService);
 
 
   //add object to notes field
-  addNote(note: Note):void {
+  addNote(note: Note): void {
     this.notes.push(note);
-      console.log(this.notes);
+    this.loggerService.log(`New note added: ${note.content} on ${note.date}`);
   }
+
+  //delete note
+  deleteNote(idx: number): void {
+    if (idx >= 0 && idx < this.notes.length) {
+      const deletedNote = this.notes.splice(idx, 1)[0];
+      this.loggerService.log(`The note ${deletedNote.content} on ${deletedNote.date} is deleted.`)
+    }
+
+  }
+
   //get notes field
   getNotes(): Note[] {
     return [...this.notes]
   }
-  //delete note
-  deleteNote(idx:number):void {
-    this.notes.splice(idx, 1);
-  }
-  
+
 }
